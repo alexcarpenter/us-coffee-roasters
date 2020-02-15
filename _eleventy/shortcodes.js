fee3e7ea05fc05597cfd23bee5e3be90b18059a9
Alex Carpenter <alex.carpenter@nationbuilder.com>
2020-02-15 09:50:44 -0500

    Feature/interviews (#24)
    * Initial interviews setup
    
    * Social updates
    
    * Mobile styles
    
    * Interview updates

--- _eleventy/shortcodes.js
+++ _eleventy/shortcodes.js
@@ -1,8 +1,10 @@
+const products = require('../_data/products.json');
+
 module.exports = {
   video: function(id) {
     return `<div style="--aspect-ratio: 16/9;">
               <iframe width="560" height="315" src="https://www.youtube.com/embed/${id}" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
-            </div>`
+            </div>`;
   },
 
   figure: function({ filename, alt = '', caption = '', ratio = '16/9' }) {
@@ -11,6 +13,29 @@ module.exports = {
                 <img src="/assets/images/${filename}" alt="${alt ? alt : ''}" />
               </div>
               ${caption ? `<figcaption>${caption}</figcaption>` : ''}
-            </figure>`
-  }
-}
+            </figure>`;
+  },
+
+  getProducts: function(ids = []) {
+    const filteredProducts = products.filter(x => ids.includes(x.id));
+    return `
+        <div class="o-grid" style="--min-column: 220px;">
+          ${filteredProducts
+            .map(
+              item =>
+                `<article class="c-card" id="product-${item.id}">
+                  <div style="--aspect-ratio: 1/1;"><img src="${item.thumbnail}" alt="Photo of ${item.name}" loading="lazy" /></div>
+                  <h3 class="c-card__heading"><a href="${item.link}">${item.name}</a></h3>
+                  <p class="c-card__description">${item.description}</p>
+                  <span class="c-card__faux-anchor">
+                    Buy on Amazon
+                    <svg aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24"><path d="M0 0h24v24H0z" fill="none"></path><path fill="currentcolor" d="M12 4l-1.41 1.41L16.17 11H4v2h12.17l-5.58 5.59L12 20l8-8z"></path></svg>
+                  </span>
+                </article>
+              `,
+            )
+            .join('')}
+        </div>
+      `;
+  },
+};
---
+++ interviews/2020-02-15-alex-carpenter.md
@@ -0,0 +1,25 @@
+---
+title: Alex Carpenter
+location: Michigan
+occupation: UI Engineer
+image: '/images/alex-carpenter.jpg'
+social:
+  instagram: 'alexcarp'
+  twitter: 'hybrid_alex'
+  website: 'https://alexcarpenter.me'
+products:
+  - 1
+  - 2
+  - 4
+  - 8
+---
+
+My passion with brewed coffee came about when I first started dating my wife. At the time, she was a full time barista at a coffee shop focused on pour overs and carefully crafted espresso drinks. Since then we have been able to take those practices from her former job and implement them into our daily coffee routine.
+
+## Mornings
+
+Each morning while I wait for my water to boil on the stove I fire up our [Breville Barista Touch](#product-8) espresso machine and pull a shot of espresso before sitting down to a brewed coffee. The [Chemex](#product-1) has been our pour over device of choice for the two of us which produces a cup for each of us.
+
+## Afternoons
+
+In the afternoon when I need a quick little pick-me-up to get through the rest of the work day I will either pull another shot of espresso or pull out our [Kalita Wave](#product-2) single cup dripper and pour myself a cup of brewed coffee.
