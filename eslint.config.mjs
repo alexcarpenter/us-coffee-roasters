bbd8c0fefeb254a516b720c4d3ac250efe25c26a
Alex Carpenter <alex.carpenter@clerk.dev>
2026-01-01 15:50:45 -0500

    add eslint and prettier

---
+++ eslint.config.mjs
@@ -0,0 +1,19 @@
+import js from '@eslint/js'
+import globals from 'globals'
+
+export default [
+  js.configs.recommended,
+  {
+    languageOptions: {
+      ecmaVersion: 2022,
+      sourceType: 'module',
+      globals: {
+        ...globals.node
+      }
+    },
+    rules: {
+      'no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
+      'no-console': 'off'
+    }
+  }
+]
--- index.d.ts
+++ index.d.ts
@@ -1,56 +1,56 @@
 export type State =
-  | "Alabama"
-  | "Arizona"
-  | "Arkansas"
-  | "California"
-  | "Colorado"
-  | "Delaware"
-  | "Florida"
-  | "Georgia"
-  | "Idaho"
-  | "Illinois"
-  | "Indiana"
-  | "Iowa"
-  | "Kansas"
-  | "Kentucky"
-  | "Louisiana"
-  | "Maine"
-  | "Maryland"
-  | "Massachusetts"
-  | "Michigan"
-  | "Minnesota"
-  | "Mississippi"
-  | "Missouri"
-  | "Montana"
-  | "Nebraska"
-  | "Nevada"
-  | "New Hampshire"
-  | "New Jersey"
-  | "New Mexico"
-  | "New York"
-  | "North Carolina"
-  | "Ohio"
-  | "Oklahoma"
-  | "Oregon"
-  | "Pennsylvania"
-  | "Rhode Island"
-  | "South Carolina"
-  | "Tennessee"
-  | "Texas"
-  | "Utah"
-  | "Vermont"
-  | "Virginia"
-  | "Washington"
-  | "Wisconsin"
-  | "Wyoming";
+  | 'Alabama'
+  | 'Arizona'
+  | 'Arkansas'
+  | 'California'
+  | 'Colorado'
+  | 'Delaware'
+  | 'Florida'
+  | 'Georgia'
+  | 'Idaho'
+  | 'Illinois'
+  | 'Indiana'
+  | 'Iowa'
+  | 'Kansas'
+  | 'Kentucky'
+  | 'Louisiana'
+  | 'Maine'
+  | 'Maryland'
+  | 'Massachusetts'
+  | 'Michigan'
+  | 'Minnesota'
+  | 'Mississippi'
+  | 'Missouri'
+  | 'Montana'
+  | 'Nebraska'
+  | 'Nevada'
+  | 'New Hampshire'
+  | 'New Jersey'
+  | 'New Mexico'
+  | 'New York'
+  | 'North Carolina'
+  | 'Ohio'
+  | 'Oklahoma'
+  | 'Oregon'
+  | 'Pennsylvania'
+  | 'Rhode Island'
+  | 'South Carolina'
+  | 'Tennessee'
+  | 'Texas'
+  | 'Utah'
+  | 'Vermont'
+  | 'Virginia'
+  | 'Washington'
+  | 'Wisconsin'
+  | 'Wyoming'
 
 export interface Roaster {
-  name: string;
-  state: State;
-  address: string;
-  website: string;
+  name: string
+  state: State
+  address: string
+  website: string
 }
 
-declare const roasters: Roaster[];
+declare const roasters: Roaster[]
 
-export default roasters;
+export default roasters
